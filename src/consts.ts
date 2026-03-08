3e97c2cf022e7c25c723f4ee214a8002352e7675
mahewses <mahewses@example.com>
2026-03-08 16:07:36 -0500

    initial commit: an absolute website setup

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'an absolute website from nowhere to no one';
+export const SITE_DESCRIPTION = 'this is an absolte website. you have probably never seen such a thing.';
